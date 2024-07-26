b323986cac68d7fa7db308ae207d2bd0ecb2dd2b
Andres Eduardo Chavez Guardo <aeguardo1@stefanini.com>
2024-07-26 16:17:51 -0500

    Se sube programa principal base

---
+++ Codigo/Prueba/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Digite un número");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Digite otro número");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("La suma es: {0}", (num1 + num2).ToString());
+            Console.ReadKey();
+        }
+    }
+}
