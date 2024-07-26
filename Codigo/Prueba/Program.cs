6136e3ed2e8de8477be519c16bdeb3ef72129bd8
Andres Eduardo Chavez Guardo <aeguardo1@stefanini.com>
2024-07-26 16:29:21 -0500

    Se agrega atributo a la clase estudiante en nueva rama

--- Codigo/Prueba/Program.cs
+++ Codigo/Prueba/Program.cs
@@ -21,8 +21,11 @@ namespace Prueba
             Console.WriteLine("Escriba su cedula");
             estudiante.Cedula = Console.ReadLine();
             Console.WriteLine();
+            Console.WriteLine("Escriba su edad");
+            estudiante.Edad = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine("El estudiante digitado es de nombre {0} con apellido {1} y cedula {2}", estudiante.Nombre, estudiante.Apellido, estudiante.Cedula);
+            Console.WriteLine();
+            Console.WriteLine("El estudiante digitado es de nombre {0} con apellido {1}, cedula {2} y edad {3}", estudiante.Nombre, estudiante.Apellido, estudiante.Cedula, estudiante.Edad);
             Console.ReadKey();
         }
     }
